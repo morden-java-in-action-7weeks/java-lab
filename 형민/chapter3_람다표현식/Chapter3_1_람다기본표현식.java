ba02d6eac076cb749e309d48e451e28f54a49534
Hyeongmin Park <thalsal@naver.com>
2022-08-17 17:47:41 +0900

    feat : chapter3 예제 코드 추가

--- 형민/chapter3_람다표현식/Chapter3_1_람다기본표현식.java
+++ 형민/chapter3_람다표현식/Chapter3_1_람다기본표현식.java
@@ -21,5 +21,22 @@ public class Chapter3_1_람다기본표현식 {
      */
     Comparator<Apple> byWeight_Lambda = (Apple a1, Apple a2) -> a1.getWeight().compareTo(a2.getWeight());
 
+    /**
+     * 람다 예시
+     */
+
+    //람다는 return이 함축되어 있음
+     int slen = (String s) -> s.length();
+
+     //람다 바디 중괄호 안에 여러 행의 문장을 포함할 수 있음
+    (int x, int y) -> {
+        System.out.println("순서대로");
+        System.out.println(x + y);
+        System.out.println("실행해요");
+    }
+
+    //람다의 파라미터가 없을 수도 있다. 이때는 42를 그대로 반환
+    () -> 42
+
 
 }
