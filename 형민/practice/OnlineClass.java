e382815d48183ed50a40dd351580832474b4bda8
Park Hyeongmin <thalsal@naver.com>
2022-09-01 17:02:21 +0900

    Hmin 문제풀이 (#18)
    * copy : 문제 카피
    
    * feat : 문제풀이;

---
+++ 형민/practice/OnlineClass.java
@@ -0,0 +1,42 @@
+package practice;
+
+public class OnlineClass {
+
+    private Integer id;
+
+    private String title;
+
+    private boolean closed;
+
+    public OnlineClass(Integer id, String title, boolean closed) {
+        this.id = id;
+        this.title = title;
+        this.closed = closed;
+    }
+
+    // Getter
+    public Integer getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public boolean isClosed() {
+        return closed;
+    }
+
+    // Setter
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setClosed(boolean closed) {
+        this.closed = closed;
+    }
+}
